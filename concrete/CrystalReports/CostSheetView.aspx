309b093665627e4633160359c7cfcac854d684e7
ramanbains <er.ramanbains@gmail.com>
2014-04-04 15:22:52 -0400

    crystal report

---
+++ concrete/CrystalReports/CostSheetView.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CostSheetView.aspx.cs" Inherits="concrete.Reports.CostSheetView" meta:resourcekey="PageResource1" UICulture="auto" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head id="Head1" runat="server">
+  <title>View Report</title>
+  <script lang="javaScript" type="text/javascript" src="crystalreportviewers13/js/crviewer/crv.js"></script> 
+        </head>
+<body>
+   <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        
+      
+        
+       <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" ReportSourceID="CrystalReportSource1" />
+       <CR:CrystalReportSource ID="CrystalReportSource1" runat="server">
+           
+       </CR:CrystalReportSource>
+        
+      
+        
+   </form>
+</body>
+</html>
---
+++ concrete/CrystalReports/CostSheetView.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using CrystalDecisions.Web;
+using CrystalDecisions.CrystalReports.Engine;
+using concrete.Services;
+
+namespace concrete.Reports
+{
+    public partial class CostSheetView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConcreteCardsService CCS = new ConcreteCardsService();
+            var rptSource = CCS.Getcarddata().ToString();
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Server.MapPath("~/CrystalReports/")+"CostSheetReport.rpt");
+            if (rptSource != null && rptSource.GetType().ToString() != "System.String")
+               // rd.SetDataSource(rptSource);
+            CrystalReportViewer1.ReportSource = rd;
+        }
+        protected void CrystalReportViewer1_Init(object sender, EventArgs e)
+        {  
+        }
+
+        protected void TextBoxDate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
